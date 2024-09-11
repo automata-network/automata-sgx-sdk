7f4463a9d0b7926855c95ab514e617d11f3a8cd5
ChenYe <git@0xdf.com>
2024-09-11 21:24:42 +0800

    fix locate teaclave path

--- build_dep.rs
+++ build_dep.rs
@@ -8,11 +8,7 @@ pub fn get_teaclave_sdk_path(manifest_path: PathBuf) -> Option<PathBuf> {
     }
 
     let mut cmd = Command::new(std::env::var("CARGO").unwrap());
-    cmd.args([
-        "metadata",
-        "--manifest-path",
-        manifest_path.as_os_str().to_str().unwrap(),
-    ]);
+    cmd.args(["metadata"]);
     let output = cmd.output().unwrap();
     let output = String::from_utf8_lossy(&output.stdout);
     let github_name = "incubator-teaclave-sgx-sdk-9a654826af166474/";
